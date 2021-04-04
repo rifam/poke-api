d5578fc945d5983ea14f9b3e5f8ac7ed2e297dc1
rifam <vinicius.rifam@gmail.com>
2021-04-04 17:55:02 -0300

    removendo arquivos de teste

---
+++ src/Pokedex.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react"
+import { AppBar, Card, CardContent, CardMedia, CircularProgress, Grid, Toolbar, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles";
+import { toFirstCharUppercase } from "./constants";
+import axios from "axios"
+
+const useStyles = makeStyles({
+    pokedexContainer: {
+        paddingTop: "20px",
+        paddingLeft:"50px",
+        paddingRight:"50px",
+    },
+    cardMedia:{
+        margin: "auto",
+    },
+    cardContent:{
+        textAlign: "center",
+    },
+});
+
+
+const Pokedex = (props) => {
+  const classes = useStyles();
+  const { history } = props;
+  const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+      });
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const getPokemonCard = (pokemonId) => {
+    const { id, name, sprite } = pokemonData[pokemonId];
+    return (
+      <Grid item xs={4} key={pokemonId}>
+        <Card onClick={() => history.push(`/${id}`)}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={sprite}
+            style={{ width: "130px", height: "130px" }}
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
+
+
+    return(
+        <>
+         <AppBar position="static">
+             <Toolbar />
+         </AppBar>
+        {pokemonData ? (
+            <Grid container spacing={2} className={classes.pokedexContainer} >
+                {Object.keys(pokemonData).map(
+                    (pokemonId) =>
+                    getPokemonCard(pokemonId)
+                )}
+            </Grid>
+        ) : (
+            <CircularProgress />
+        )}
+         
+           
+        </>
+    )
+}
+
+
+export default Pokedex;
